fc14463a9643f42913fdec826409dba68a522e91
Crowdbotics <team@crowdbotics.com>
2024-10-25 09:18:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-wildflower-49808",
+  url: "https://lucky-wildflower-49808.botics.co",
+  api: "https://lucky-wildflower-49808.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
